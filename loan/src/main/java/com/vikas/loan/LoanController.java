53ce17b8d75031f32dfda73fc82dafd9ffcfae18
Vikas Yadav <do.vikas1369@gmail.com>
2024-02-11 20:27:56 +0000

    Added dependencies to model

--- loan/src/main/java/com/vikas/loan/LoanController.java
+++ loan/src/main/java/com/vikas/loan/LoanController.java
@@ -1,5 +1,6 @@
 package com.vikas.loan;
 
+import com.vikas.attributemodel.model.ProfileAttributes;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,15 @@ public class LoanController {
 
     @GetMapping("/num")
     public Integer getLoanAmount(){
+
         return 20;
+    }
+
+    @GetMapping("/attribute")
+    public ProfileAttributes getAttr(){
+        ProfileAttributes profileAttributes = new ProfileAttributes();
+        profileAttributes.setLePay("123");;
+        profileAttributes.setRehypothecation("5325");
+        return  profileAttributes;
     }
 }
